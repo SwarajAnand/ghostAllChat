30fb61b0a36a74b82d362f86788dedc154a20cb6
Swaraj Anand <swrjnnd@gmail.com>
2024-05-26 21:11:02 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import ChatRoom from "./pages/ChatRoom";
+import Login from "./pages/Login";
+import PrivateRoute from "./routes/PrivateRoutes.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { AuthProvider } from './context/AuthContext.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Login />,
+      },
+      {
+        path: '/chat',
+        element: (
+          <PrivateRoute>
+            <ChatRoom />
+          </PrivateRoute>
+        ),
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+  <RouterProvider router={router} />
+  </AuthProvider>
+)
